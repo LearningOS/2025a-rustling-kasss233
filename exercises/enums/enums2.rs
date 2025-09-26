1018b320a195d71a373289a4e8ad24cc070a88a2
kasss233 <1592858973@qq.com>
2025-09-26 20:35:50 +0800

    1

--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -8,6 +8,10 @@
 #[derive(Debug)]
 enum Message {
     // TODO: define the different variants used below
+    Move{x:i32,y:i32},
+    Echo(String),
+    ChangeColor(i8,i8,i8),
+    Quit
 }
 
 impl Message {
